651882d6906b9c59da928c670211d9bc2375a6d5
Elizabeth Anatskaya <anatskayalisa@gmail.com>
2017-01-05 13:57:46 +0300

    structure

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var rename = require('gulp-rename');
+var runSequence = require('gulp-run-sequence');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var mqpacker = require('css-mqpacker');
+
+gulp.task('style', function() {
+  gulp.src('sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        'last 2 versions'
+      ]}),
+      mqpacker({
+        sort: true
+      })
+    ]))
+    .pipe(gulp.dest('css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+
+    .pipe(server.stream());
+});
+
+gulp.task('symbols', function() {
+  return gulp.src('img/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('symbols.svg'))
+    .pipe(gulp.dest('img'));
+});
+
+gulp.task('serve', ['style'], function() {
+  server.init({
+    server: '.',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('*.html').on('change', server.reload);
+});
